a1653b8525aa5baccea08afa0c4c70a648aeec29
Matthew Wise <matthew.rs.wise@gmail.com>
2020-01-09 09:22:35 -0800

    initial java version based on pythonic examples in readme

---
+++ java/src/main/java/com/matt_wise/flyover/Point.java
@@ -0,0 +1,10 @@
+package com.matt_wise.flyover;
+
+public class Point {
+    public double x;
+    public double y;
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
